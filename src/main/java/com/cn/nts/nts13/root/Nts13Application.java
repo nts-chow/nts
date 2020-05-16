ebe891987be9668e2da8e5eeccfe2545ebfacb53
andre <jiuzhou.zhou9@gmail.com>
2020-05-16 23:13:21 +0900

    andre

--- src/main/java/com/cn/nts/nts13/root/Nts13Application.java
+++ src/main/java/com/cn/nts/nts13/root/Nts13Application.java
@@ -1,11 +1,12 @@
 package com.cn.nts.nts13.root;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(basePackages = "com.cn.nts.nts13.root.dao")
 @RestController
@@ -18,5 +19,4 @@ public class Nts13Application {
     public static void main(String[] args) {
         SpringApplication.run(Nts13Application.class, args);
     }
-
 }
--- src/test/java/com/cn/nts/nts13/root/Nts13DaoTests.java
+++ src/test/java/com/cn/nts/nts13/root/Nts13DaoTests.java
@@ -1,17 +1,23 @@
 package com.cn.nts.nts13.root;
 
 import com.cn.nts.nts13.root.dao.EmpMapper;
+import com.cn.nts.nts13.root.dao.ItemsMapper;
 import com.cn.nts.nts13.root.entity.Emp;
+import com.cn.nts.nts13.root.entity.Items;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
 
-@SpringBootTest(classes =Nts13Application.class)
+@SpringBootTest(classes = Nts13Application.class)
 @Transactional
 @Rollback
 class Nts13DaoTests {
@@ -19,12 +25,37 @@ class Nts13DaoTests {
     @Autowired
     private EmpMapper empMapper;
 
-    @Test
-    public void testSelect(){
-       List<Emp> empList=empMapper.selectAll();
+    @Autowired
+    private ItemsMapper itemsMapper;
 
-        empList.forEach((item)->{
-            System.out.println("######"+item.getName()+"######");
+    @Test
+    public void testSelect() {
+        List<Emp> empList = empMapper.selectAll();
+
+        empList.forEach((item) -> {
+            System.out.println("######" + item.getName() + "######");
+        });
+    }
+
+    @Test
+    public void testPage() {
+        PageHelper.startPage(1, 2);
+
+        List<Items> list = itemsMapper.selectAll();
+        list.forEach((items) -> {
+            System.out.println(items.getName());
+        });
+        //创建一个PageInfo对象,传入泛型类
+        PageInfo<Items> pageInfo = new PageInfo<>();
+
+        pageInfo.setList(list);
+        list=pageInfo.getList();
+//        Example ex = new Example(Items.class);
+//        PageInfo<Items> pageInfo = (PageInfo<Items>) itemsMapper.selectByExample(ex);
+//        List<Items> list = pageInfo.getList();
+
+        list.forEach((items) -> {
+            System.out.println("##########"+items.getName());
         });
     }
 }
