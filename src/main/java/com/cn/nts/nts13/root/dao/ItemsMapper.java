e061ac7e3050ed76ce6b4d0a1bd070dbda6fd62f
andre <jiuzhou.zhou9@gmail.com>
2020-05-17 10:19:44 +0900

    har

--- src/main/java/com/cn/nts/nts13/root/dao/ItemsMapper.java
+++ src/main/java/com/cn/nts/nts13/root/dao/ItemsMapper.java
@@ -1,6 +1,8 @@
 package com.cn.nts.nts13.root.dao;
 
 import com.cn.nts.nts13.root.entity.Items;
+import tk.mybatis.mapper.entity.Example;
+
 import java.util.List;
 
 public interface ItemsMapper {
@@ -13,4 +15,6 @@ public interface ItemsMapper {
     List<Items> selectAll();
 
     int updateByPrimaryKey(Items record);
+
+    List<Items> selectByExample(Example example);
 }
